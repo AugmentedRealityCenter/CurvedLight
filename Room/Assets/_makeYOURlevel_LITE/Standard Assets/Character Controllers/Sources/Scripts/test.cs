a87ba4267e3c856a869e525c2d4a6eeb47fe8c1a
zhongn2 <zhongn2@miamioh.edu>
2014-03-17 19:18:36 -0400

    commit
    I really don't know whether it will work or not

---
+++ Room/Assets/_makeYOURlevel_LITE/Standard Assets/Character Controllers/Sources/Scripts/test.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	public MeshFilter MF;
+	
+	void Start()
+	{
+		Mesh m = GameObject.FindObjectOfType (MeshFilter);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
